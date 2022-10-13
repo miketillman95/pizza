5d8157d4b9b869f485a60337e4ad1f993731b92f
mike <matillman95@gmail.com>
2022-10-13 14:22:30 -0400

    im lost && git push

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const server = express()
-const port = 3010
+const server_port = process.env.PORT || 3010;
+const server_host =  '0.0.0.0';
 const cors = require('cors')
 const pizzaRouter = require('./db/api/pizza/pizza-router')
 const toppingsRouter = require('./db/api/toppings/toppings-router')
@@ -15,6 +16,6 @@ server.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-server.listen(process.env.PORT, port, () => {
-  console.log(process.env.PORT || `Example app listening on port ${port}`)
+server.listen(server_port, server_host, () => {
+  console.log('Listening on port %d', server_port)
 })
