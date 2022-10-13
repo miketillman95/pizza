1fa18737ac3aee06435df0385c05fd9dc0900653
mike <matillman95@gmail.com>
2022-10-13 14:33:52 -0400

    help

--- db/pgDb.js
+++ db/pgDb.js
@@ -3,7 +3,7 @@ const {Client} = require('pg')
 const client = new Client({
     host: 'localhost',
     user: 'postgres',
-    port: process.env.PORT,
+    port: 5432,
     password: process.env.PASSWORD,
     database: 'postgres'
 
