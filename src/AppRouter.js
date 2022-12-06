5911494c46f7c006ff8e80c9312601bae576fcfc
mike <matillman95@gmail.com>
2022-12-06 15:26:12 -0700

    deleted extra files

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,9 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import EditToppings from './components/Edits/EditToppings';
 import Home from './components/Home'
 import Owner from './components/Owner';
 import Chef from './components/Chef'
-import EditPizza from './components/Edits/EditPizza';
 import UpdatePizza from './components/Edits/UpdatePizza'
 import UpdateToppings from './components/Edits/UpdateToppings'
 
@@ -16,8 +14,6 @@ function AppRouter() {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/owner' element={<Owner/>}/>
-      <Route path='/edittoppings' element={<EditToppings/>}></Route>
-      <Route path='/editpizza' element={<EditPizza/>}></Route>
       <Route path='/chef' element={<Chef/>}/>
       <Route path='/updatepizza' element={<UpdatePizza/>}/>
       <Route path='/updatetoppings' element={<UpdateToppings/>}/>
